89c99ea47c9e83239903e0686c6a08145c1c38c9
hermannpoilpre <hermannpoilpre@gmail.com>
2016-03-02 23:11:26 +0100

    Modification des taches Ajout des dépendances front Ajout des less

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -20,48 +20,24 @@ var cssFilesToInject = [
   'styles/**/*.css'
 ];
 
-
-// Client-side javascript files to inject in order
-// (uses Grunt-style wildcard/glob/splat expressions)
 var jsFilesToInject = [
-
-  // Load sails.io before everything else
   'js/dependencies/sails.io.js',
 
-  // Dependencies like jQuery, or Angular are brought in here
-  'js/dependencies/**/*.js',
+  "js/dependencies/jquery/dist/jquery.min.js",
+  "js/dependencies/angular/angular.min.js",
+  "js/dependencies/angular-ui-router/release/angular-ui-router.min.js",
+  "js/dependencies/angular-animate/angular-animate.min.js",
+  "js/dependencies/angular-materialize/src/angular-materialize.js",
 
-  // All of the rest of your client-side js files
-  // will be injected here in no particular order.
   'js/**/*.js'
 ];
 
-
-// Client-side HTML templates are injected using the sources below
-// The ordering of these templates shouldn't matter.
-// (uses Grunt-style wildcard/glob/splat expressions)
-//
-// By default, Sails uses JST templates and precompiles them into
-// functions for you.  If you want to use jade, handlebars, dust, etc.,
-// with the linker, no problem-- you'll just want to make sure the precompiled
-// templates get spit out to the same file.  Be sure and check out `tasks/README.md`
-// for information on customizing and installing new tasks.
 var templateFilesToInject = [
   'templates/**/*.html'
 ];
 
-
-
-
-
-
-
-// Default path for public folder (see documentation for more information)
 var tmpPath = '.tmp/public/';
 
-// Prefix relative paths to source files so they point to the proper locations
-// (i.e. where the other Grunt tasks spit them out, or in some cases, where
-// they reside in the first place)
 module.exports.cssFilesToInject = cssFilesToInject.map(function(cssPath) {
   return require('path').join('.tmp/public/', cssPath);
 });
